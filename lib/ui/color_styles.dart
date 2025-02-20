049c974058d35bc2a5dfb9f60ab42883f63073ea
bradjjc <jichangjung@yahoo.com>
2025-02-20 19:24:21 +0900

    text 와 color 의 style 정의

---
+++ lib/ui/color_styles.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorStyles {
+  static const Color black = Color(0xff000000);
+  static const Color gray1 = Color(0xff484848);
+  static const Color gray2 = Color(0xff797979);
+  static const Color gray3 = Color(0xffA9A9A9);
+  static const Color gray4 = Color(0xffD9D9D9);
+
+  static const Color white = Color(0xffFFFFFF);
+
+  static const Color primary100 = Color(0xff129575);
+  static const Color primary80 = Color(0xff71B1A1);
+  static const Color primary60 = Color(0xffAFD3CA);
+  static const Color primary40 = Color(0xffDBEBE7);
+  static const Color primary20 = Color(0xffF6FAF9);
+
+  static const Color secondary100 = Color(0xffff9c00);
+  static const Color secondary80 = Color(0xffffa61a);
+  static const Color secondary60 = Color(0xffffba4d);
+  static const Color secondary40 = Color(0xffffce80);
+  static const Color secondary20 = Color(0xffffe1b3);
+
+  static const Color error = Color(0xffFD3654);
+  static const Color warning = Color(0xffFFE1E7);
+
+  static const Color success = Color(0xff31b057);
+  static const Color success10 = Color(0xffEAF7EE);
+
+  static const Color rating = Color(0xffffad30);
+
+  static const Color primaryColor = primary100;
+  static const Color secondaryColor = secondary100;
+  static const Color labelColor = Color(0xff121212);
+}
